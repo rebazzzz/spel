b09090cc6258735247da8cd3f61b3161ca0835ac
rebazzzz <rebaz.abdulrashid@elev.ga.ntig.se>
2023-12-19 10:08:18 +0100

    fixade hp-output

--- spel.py
+++ spel.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 enemy_hp = 80
 player_hp = 100
@@ -7,28 +7,42 @@ sword2 = 25
 sword3 = 30
 
 def spelare_attack():
+    """Spelaren attackerar enemy genom att välja vapen"""
     global enemy_hp
-    attack = int(input("Vilket vapen vill du använda Svärd1, svärd2 eller svärd3? Skriv 1, 2 eller 3:"))
+    attack = int(input("Vilket vapen vill du använda Svärd1, svärd2 eller svärd3? Skriv 1, 2 eller 3 \n"))
 
     if attack == 1:
         enemy_hp -= sword1
-        print(f"Enemy har nu {enemy_hp} hp efter din attack.")
+        if enemy_hp < 0:
+            print(f"Enemy har nu 0 hp efter din attack.")
+        else:
+            print(f"Enemy har nu {enemy_hp} hp efter din attack.")
     elif attack == 2:
         enemy_hp -= sword2 
-        print(f"Enemy har nu {enemy_hp} hp efter din attack.")
+        if enemy_hp < 0:
+            print(f"Enemy har nu 0 hp efter din attack.")
+        else:
+            print(f"Enemy har nu {enemy_hp} hp efter din attack.")
     elif attack == 3:
         enemy_hp -= sword3
-        print(f"Enemy har nu {enemy_hp} hp efter din attack.")
+        if enemy_hp < 0:
+            print(f"Enemy har nu 0 hp efter din attack.")
+        else:
+            print(f"Enemy har nu {enemy_hp} hp efter din attack.")
     else:
         print("Använd rätt vapen!")
 
 def enemy_attack():
+    """Enemy attackerar spelaren med random damage"""
     global player_hp
-    damage = randint(10, 20)
+    damage = random.randint(10, 20)
     player_hp -= damage
-    print(f"Du har nu {player_hp} hp efter enemy attack.")
+    if player_hp < 0:
+        print(f"Du har nu 0 hp efter enemy attack.")
+    else:
+        print(f"Du har nu {player_hp} hp efter enemy attack.")
 
-while player_hp > 0 and enemy_hp > 0:
+while player_hp > 0 or enemy_hp > 0:
     spelare_attack()
     if enemy_hp <= 0:
         print("Enemy är död")
@@ -38,4 +52,3 @@ while player_hp > 0 and enemy_hp > 0:
     if player_hp <= 0:
         print("GAME OVER")
         break
-
