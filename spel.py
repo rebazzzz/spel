89c2e16a79492c5f002519d28e0200a1b3f58da3
rebazzzz <rebaz.abdulrashid@elev.ga.ntig.se>
2023-12-19 11:02:42 +0100

    la till emoji och mer details

--- spel.py
+++ spel.py
@@ -6,7 +6,7 @@ player_hp = 120
 pickaxe = 25
 yxa = 30
 magisktlubba = 35
-arsenal = ["pickaxe", "yxa", "magisktlubba"]
+arsenal = ["pickaxe 🪓", "yxa 🔨", "magisktlubba 🍭✨"]
 
 
 def spelare_attack():
@@ -19,19 +19,20 @@ def spelare_attack():
         if enemy_hp < 0:
             print(f"Enemy har nu 0 hp efter din attack.")
         else:
-            print(f"Enemy har nu {enemy_hp} hp efter din attack.")
+            print(f"🦴💥: Enemy har nu {enemy_hp} hp efter din attack.")
     elif attack == 2:
         enemy_hp -= yxa
         if enemy_hp < 0:
             print(f"Enemy har nu 0 hp efter din attack.")
         else:
-            print(f"Enemy har nu {enemy_hp} hp efter din attack.")
+            print(f"🦴💥💥: Enemy har nu {enemy_hp} hp efter din attack.")
     elif attack == 3:
         enemy_hp -= magisktlubba
         if enemy_hp < 0:
             print(f"Enemy har nu 0 hp efter din attack.")
         else:
-            print(f"Enemy har nu {enemy_hp} hp efter din attack.")
+            print(f"🦴💥💥💥: Enemy har nu {enemy_hp} hp efter din attack.")
+            #⚔️🦴💥
     else:
         print("Använd rätt vapen!")
 
@@ -43,7 +44,7 @@ def enemy_attack():
     if player_hp < 0:
         print(f"Du har nu 0 hp efter enemy attack.")
     else:
-        print(f"Du har nu {player_hp} hp efter enemy attack.")
+        print(f"💥💥💥: Du har nu {player_hp} hp efter enemy attack.")
 
 print (f"""Du har dessa vapen i din arsenal: {arsenal}. Din pickaxe gör 25 damage, yxan gör 30 och den magiska klubban gör 35!
 Vilket vapen vill du attackera skeletten med?""")
@@ -57,3 +58,5 @@ while player_hp > 0 or enemy_hp > 0:
     if player_hp <= 0:
         print("GAME OVER")
         break
+
+
